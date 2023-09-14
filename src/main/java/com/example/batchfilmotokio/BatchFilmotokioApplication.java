4eeb0ba825b125890bd691d9013dcde27b9d5322
Daniel <d.andres.bacho@gmail.com>
2023-09-14 11:39:01 +0200

    Initial commit

---
+++ src/main/java/com/example/batchfilmotokio/BatchFilmotokioApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchfilmotokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchFilmotokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchFilmotokioApplication.class, args);
+	}
+
+}
